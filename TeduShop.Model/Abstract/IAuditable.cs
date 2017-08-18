d364d72213b9451c216167c9d279cd89d3d3b478
User-64-PC\User-64 <hoanghuongha1208@gmail.com>
2017-08-18 11:50:27 +0700

    add model

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+	public interface IAuditable
+	{
+		DateTime? CreateDate { set; get; }
+		string CreateBy { set; get; }
+		DateTime? UpdatedDate { set; get; }
+		string UpdatedBy { set; get; }
+
+		string MetaKeyword { set; get; }
+		string MetaDescription { set; get; }
+
+		bool Status { set; get; }
+
+	}
+}
