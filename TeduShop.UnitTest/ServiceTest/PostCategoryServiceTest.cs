a485cc146f1d38c07fa403e8dffba81ddd34e0f4
User-64-PC\User-64 <hoanghuongha1208@gmail.com>
2017-09-01 19:33:46 +0700

    add unit test

---
+++ TeduShop.UnitTest/ServiceTest/PostCategoryServiceTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+using TeduShop.Service;
+
+namespace TeduShop.UnitTest.ServiceTest
+{
+	[TestClass]
+	public class PostCategoryServiceTest
+	{
+		private Mock<IPostCategoryRepository> _mockRepository;
+		private Mock<IUnitOfWork> _mockUnitOfWork;
+		private IPostCategoryService _categoryService;
+		private List<PostCategory> _listCategory;
+
+		[TestInitialize]
+		public void Initiallize()
+		{
+			_mockRepository = new Mock<IPostCategoryRepository>();
+			_mockUnitOfWork = new Mock<IUnitOfWork>();
+			_categoryService = new PostCategoryService(_mockRepository.Object, _mockUnitOfWork.Object);
+			_listCategory = new List<PostCategory>()
+			{
+				new PostCategory() {ID = 1, Name="DM1", Status=true },
+				new PostCategory() {ID = 2, Name="DM1", Status=true },
+			};
+		}
+		[TestMethod]
+		public void PostCategory_Service_GetAll()
+		{
+			// setup method
+			_mockRepository.Setup(m => m.GetAll(null)).Returns(_listCategory);
+			//call action
+			var result =_categoryService.GetAll() as List<PostCategory>;
+			Assert.IsNotNull(result);
+			Assert.AreEqual(2, result.Count);
+		}
+
+		[TestMethod]
+		public void PostCategory_Service_Create()
+		{
+			int id = 1;
+			PostCategory category = new PostCategory();
+			category.Name = "Test";
+			category.Alias = "test";
+			category.Status = true;
+			_mockRepository.Setup(m => m.Add(category)).Returns((PostCategory p) => 
+			{
+				p.ID = 1;
+				return p;
+			});
+			var result = _categoryService.Add(category);
+
+			Assert.IsNotNull(result);
+			Assert.AreEqual(1, result.ID);
+
+		}
+	}
+}
